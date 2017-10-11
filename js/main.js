cb2cd5b9bf7c8b677ad838f4ae16c6e6d150f697
EmmaKoza <emma.koza@gmail.com>
2017-10-10 20:35:11 -0400

    psuedo code and some fake sentences

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
 "use strict";
 
 var feminipsum = {};
+//array of all the sentences
+var sentences = ['sentence one words words', 'sentence three yay yaya yaya', 'heres another sentence', 'sentence yay yay yay yay'];
+
+//paragraph array var for final ipsum 
+var paragraph = [];
+
+//randomly sort through the array  
+//build the strings into sentences (capital first letter and .)
+
+//go through the array 5 times per paragraph
+
+//let the user decide how many paragraphs there will be
